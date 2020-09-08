bba969d3c4dddd24b132cb830f2d3891306b3a8c
Brian H. Ward <glyphrider@gmail.com>
2020-09-08 13:23:49 -0400

    some actual polymorphism

--- Makefile
+++ Makefile
@@ -1,9 +1,19 @@
 EXE=demo
-OBJS=main.o hello.o
+OBJS=main.o
+
+$(PHONY) : all
+all : $(EXE) libhello1.so libhello2.so
 
 $(EXE) : $(OBJS)
-	$(CC) $(LDFLAGS) -o $@ $^
+	$(CC) $(LDFLAGS) -ldl -o $@ $^
+
+libhello1.so : hello1.o
+	$(CC) $(LDFLAGS) -shared -Wl,-soname,$@ -o $@ $^
+
+libhello2.so : hello2.o
+	$(CC) $(LDFLAGS) -shared -Wl,-soname,$@ -o $@ $^
 
 $(PHONY) : clean
 clean :
 	rm -f $(EXE) $(OBJS)
+	rm -f libhello1.so libhello2.so
--- main.c
+++ main.c
@@ -1,10 +1,16 @@
-#include "hello.h"
+#include <dlfcn.h>
+
+#define SYM_NAME "say_hello"
 
 int main(int argc, char *argv[])
 {
-    void (*hello)(const char *);
-    hello = &say_hello;
+  char *libname = argc>1?argv[1]:"./libhello1.so";
 
-    (*hello)("pointer");
-    return 0;
+  void (*hello)(const char *);
+  void *lib = dlopen(libname,RTLD_LAZY);
+  hello = dlsym(lib,SYM_NAME);
+  
+  (*hello)("pointer");
+  dlclose(lib);
+  return 0;
 }
